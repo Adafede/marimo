94cea6fcd290c173ecffd8a1eea975ff23d33fd6
Adriano Rutz <adriano.rutz@hotmail.com>
2026-01-15 22:37:06 +0100

    up

--- apps/my_testing_app.py
+++ apps/my_testing_app.py
@@ -1,6 +1,7 @@
 # /// script
 # requires-python = "==3.13.*"
 # dependencies = [
+#     "httpimport==1.4.1",
 #     "marimo",
 # ]
 #
@@ -9,21 +10,23 @@
 # theme = "system"
 # ///
 
-
 import marimo
 
 __generated_with = "0.19.2"
 app = marimo.App(app_title="Package Demo")
 
 with app.setup:
-    import marimo as mo
+    import httpimport
+
+    with httpimport.remote_repo(url="https://raw.githubusercontent.com/Adafede/marimo/main/modules"):
+        # Now you can import the module
+        from text import smiles
 
 
 @app.cell
 def _():
-    mo.notebook_location()
+    smiles.validate_smiles("aaa")
     return
-
 
 if __name__ == "__main__":
     app.run()
