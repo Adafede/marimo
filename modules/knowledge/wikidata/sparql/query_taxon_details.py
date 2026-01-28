9ede5e05af32a2c3d3005b0dfba82bc7a32f16ab
Adriano Rutz <adriano.rutz@hotmail.com>
2026-01-28 03:33:47 +0100

    up

--- modules/knowledge/wikidata/sparql/query_taxon_details.py
+++ modules/knowledge/wikidata/sparql/query_taxon_details.py
@@ -10,19 +10,19 @@ def query_taxon_details(values_clause_str: str) -> str:
     return f"""
     {PREFIXES}
     SELECT
-    (xsd:integer(STRAFTER(STR(?t), "Q")) AS ?taxon)
+    ?taxon
     ?taxonLabel
     ?taxonDescription
-    (xsd:integer(STRAFTER(STR(?tp), "Q")) AS ?taxon_parent)
+    ?taxon_parent
     ?taxon_parentLabel
     WHERE {{
       {values_clause_str}
-      OPTIONAL {{ ?t wdt:P171 ?tp }}
-      OPTIONAL {{ ?t rdfs:label ?taxonLabel . FILTER(LANG(?taxonLabel) = "en") }}
-      OPTIONAL {{ ?t rdfs:label ?taxonLabel . FILTER(LANG(?taxonLabel) = "mul") }}
-      OPTIONAL {{ ?t schema:description ?taxonDescription . FILTER(LANG(?taxonDescription) = "en") }}
-      OPTIONAL {{ ?t schema:description ?taxonDescription . FILTER(LANG(?taxonDescription) = "mul") }}
-      OPTIONAL {{ ?tp rdfs:label ?taxon_parentLabel . FILTER(LANG(?taxon_parentLabel) = "en") }}
-      OPTIONAL {{ ?tp rdfs:label ?taxon_parentLabel . FILTER(LANG(?taxon_parentLabel) = "mul") }}
+      OPTIONAL {{ ?taxon wdt:P171 ?taxon_parent }}
+      OPTIONAL {{ ?taxon rdfs:label ?taxonLabel . FILTER(LANG(?taxonLabel) = "en") }}
+      OPTIONAL {{ ?taxon rdfs:label ?taxonLabel . FILTER(LANG(?taxonLabel) = "mul") }}
+      OPTIONAL {{ ?taxon schema:description ?taxonDescription . FILTER(LANG(?taxonDescription) = "en") }}
+      OPTIONAL {{ ?taxon schema:description ?taxonDescription . FILTER(LANG(?taxonDescription) = "mul") }}
+      OPTIONAL {{ ?taxon_parent rdfs:label ?taxon_parentLabel . FILTER(LANG(?taxon_parentLabel) = "en") }}
+      OPTIONAL {{ ?taxon_parent rdfs:label ?taxon_parentLabel . FILTER(LANG(?taxon_parentLabel) = "mul") }}
     }}
     """
