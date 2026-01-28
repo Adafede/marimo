a7426b6262cecb0477658f1d1a06a607b4a4d531
Adriano Rutz <adriano.rutz@hotmail.com>
2026-01-28 04:42:04 +0100

    up

--- apps/lotus_wikidata_explorer.py
+++ apps/lotus_wikidata_explorer.py
@@ -880,7 +880,9 @@ def build_display_dataframe(df: pl.LazyFrame) -> pl.DataFrame:
     # Generate molecule images (small Python loop)
     df = df.with_columns(
         pl.col("smiles")
-        .map_elements(lambda s: html_from_image(mo.image(s)) if s else "")
+        .map_elements(
+            lambda s: html_from_image(mo.image(s)), return_dtype=pl.String if s else ""
+        )
         .alias("Compound Depiction")
     )
 
