e73b65913dd44bf16ca2ac17a6376b1dc3c4694e
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2026-01-29 10:12:51 +0100

    refactor(apps/lotus_wikidata_explorer.py): update explorer settings to use remote repository by default

--- apps/lotus_wikidata_explorer.py
+++ apps/lotus_wikidata_explorer.py
@@ -50,7 +50,7 @@ with app.setup:
     from rdflib.namespace import RDF, RDFS, XSD, DCTERMS
     from abc import ABC, abstractmethod
 
-    _USE_LOCAL = True
+    _USE_LOCAL = False
     if _USE_LOCAL:
         sys.path.insert(0, ".")
 
