eb3f39c5d0457d47311cbe90e4fd1b2e34ec84dd
Adriano Rutz <adriano.rutz@hotmail.com>
2026-03-18 19:13:14 +0100

    rdflib -> maplib

--- modules/knowledge/rdf/namespace/wikidata.py
+++ modules/knowledge/rdf/namespace/wikidata.py
@@ -1,28 +1,27 @@
-"""Wikidata RDF namespaces for rdflib."""
+"""Wikidata RDF namespaces."""
 
-__all__ = ["WIKIDATA_NAMESPACES", "bind_wikidata_namespaces"]
-
-from rdflib import Graph, Namespace
+__all__ = ["WIKIDATA_NAMESPACES", "WIKIDATA_PREFIXES"]
 
 # Wikidata base URLs
 ENTITY_BASE = "http://www.wikidata.org/entity/"
 STATEMENT_BASE = "http://www.wikidata.org/entity/statement/"
 
-# RDF Namespaces for Wikidata
+# RDF Namespaces for Wikidata (as plain strings for maplib compatibility)
 WIKIDATA_NAMESPACES = {
-    "WD": Namespace(ENTITY_BASE),
-    "WDREF": Namespace(ENTITY_BASE.replace("entity/", "reference/")),
-    "WDS": Namespace(STATEMENT_BASE),
-    "WDT": Namespace(ENTITY_BASE.replace("entity/", "prop/direct/")),
-    "P": Namespace(ENTITY_BASE.replace("entity/", "prop/")),
-    "PS": Namespace(ENTITY_BASE.replace("entity/", "prop/statement/")),
-    "PR": Namespace(ENTITY_BASE.replace("entity/", "prop/reference/")),
-    "PROV": Namespace("http://www.w3.org/ns/prov#"),
-    "SCHEMA": Namespace("http://schema.org/"),
+    "wd": ENTITY_BASE,
+    "wdref": ENTITY_BASE.replace("entity/", "reference/"),
+    "wds": STATEMENT_BASE,
+    "wdt": ENTITY_BASE.replace("entity/", "prop/direct/"),
+    "p": ENTITY_BASE.replace("entity/", "prop/"),
+    "ps": ENTITY_BASE.replace("entity/", "prop/statement/"),
+    "pr": ENTITY_BASE.replace("entity/", "prop/reference/"),
+    "prov": "http://www.w3.org/ns/prov#",
+    "schema": "http://schema.org/",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    "dcterms": "http://purl.org/dc/terms/",
 }
 
-
-def bind_wikidata_namespaces(graph: Graph) -> None:
-    """Bind all Wikidata namespaces to an rdflib Graph."""
-    for prefix, ns in WIKIDATA_NAMESPACES.items():
-        graph.bind(prefix.lower(), ns)
+# For backwards compatibility with uppercase keys
+WIKIDATA_PREFIXES = {k.upper(): v for k, v in WIKIDATA_NAMESPACES.items()}
